26a1fff74c961e215fcf56791da3fac4e868a2de
Ellen Hayward <Ellen Hayward>
2016-08-20 15:39:40 +1200

    Add TradeMe API Integration, Secret key (not on git)

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise-native');
+var secret = require('../env');
+
+function requestNumListings(regionId, categoryId){
+  var options = { method: 'GET',
+    url: 'https://api.trademe.co.nz/v1/Search/Jobs.json',
+    qs: { category: categoryId, region: regionId },
+    headers:
+     {
+       authorization: secret
+     },
+     json: true
+    };
+
+  return request(options).then(data => {
+    return data.TotalCount;
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  
+  requestNumListings(100, 5001).then(data => {
+    console.log(data);
+  });
   res.render('index', { title: 'Express' });
-});
-
-router.get('/cards', function(req, res, next){
-  res.render('cards');
 });
 
 module.exports = router;
