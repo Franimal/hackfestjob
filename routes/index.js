c21be285ca725bfbd761e7b0cecee1ab36deae4e
Harman Singh <harman.singh@hotmail.co.nz>
2016-08-20 14:30:38 +1200

    added cards template

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cards', function(req, res, next){
+  res.render('cards');
+});
+
 module.exports = router;
