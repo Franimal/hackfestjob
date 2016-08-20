4f6125bda216f3fc8c8a1a6d2f03a32db266d876
Josh <jjmoona0@gmail.com>
2016-08-20 05:07:18 +0100

    Href link drop down finish

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var trademe = require('../data/trademe-ids');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  trademe. getLocationIds().then(function(locations){
+    var categories = ["accounting", "engineering", "painting"];
+    res.render('index', { title: 'Express' , categories: categories});
+  });
 });
 
 router.get('/cards', function(req, res, next){
