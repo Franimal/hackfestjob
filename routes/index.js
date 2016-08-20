a325cc9af64809849c0b3d91869720d539c9cc32
Tim King <king.timothycw@gmail.com>
2016-08-20 16:08:23 +1200

    Add regiondata to page render: data in 'regions'

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request-promise-native');
 var secret = require('../env');
+var apiIds = require('../data/trademe-ids.js');
 
 function requestNumListings(regionId, categoryId){
   var options = { method: 'GET',
@@ -23,8 +24,9 @@ function requestNumListings(regionId, categoryId){
 router.get('/', function(req, res, next) {
   requestNumListings(100, 5001).then(data => {
     console.log(data);
+    regionData = [{name:'Auckland', id:1, jobcount:500, jobratio:'1:4'}, {name:'Wellington', id:2, jobcount:90000, jobratio:'1:4'}];
   });
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', regions: regionData});
 });
 
 module.exports = router;
