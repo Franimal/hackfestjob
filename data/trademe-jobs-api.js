4c8b9f7f4059b0f14901c61acfb86d544cb5a239
Ellen Hayward <Ellen Hayward>
2016-08-21 00:07:08 +1200

    fix imports for trademe-jobs-api.js

--- data/trademe-jobs-api.js
+++ data/trademe-jobs-api.js
@@ -1,5 +1,5 @@
 var request = require('request-promise-native');
-var apiIds = require('../trademe-ids');
+var apiIds = require('./trademe-ids');
 
 function getListingCount(regionId, categoryId) {
 
