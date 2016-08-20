635789fa0551f053a2b051b7fde04d802c2870cf
David Barnett <david@barnett.net.nz>
2016-08-20 14:18:37 +1200

    add js module for regional population data

---
+++ data/region-population.js
@@ -0,0 +1,18 @@
+// usage: pop.Auckland or pop['Auckland']
+module.export = {
+  "Northland": 155805,
+  "Auckland": 1438446,
+  "Waikato": 413202,
+  "Bay of Plenty": 273360,
+  "Gisborne": 43938,
+  "Hawke's Bay": 156018,
+  "Taranaki": 110079,
+  "Manawatu-Wanganui": 225411,
+  "Wellington": 478290,
+  "Nelson / Tasman": 99702,
+  "Marlborough": 46302,
+  "West Coast": 36294,
+  "Canterbury": 554919,
+  "Otago": 223017,
+  "Southland": 97821
+}
