eb0805beb12c4141a3de78ce6af6bf32d6c5bda5
David Barnett <david@barnett.net.nz>
2016-08-20 16:18:51 +1200

    add regional unemployment and merged with population data

---
+++ data/region-data.js
@@ -0,0 +1,42 @@
+// Labour Market Statistics: June 2016 quarter
+unemployment = {
+  "Northland",9100,
+  "Auckland",41500,
+  "Waikato",11500,
+  "Bay of Plenty",7900,
+  "Gisborne",2100,
+  "Hawke's Bay",3300,
+  "Taranaki",3000,
+  "Manawatu / Wanganui",7100,
+  "Wellington",15600,
+  "Nelson / Tasman",3300,
+  "Marlborough",1400,
+  "West Coast",1000,
+  "Canterbury",11100,
+  "Otagoi",5400,
+  "Southland",3000
+};
+
+// census 2013 data
+population = {
+  "Northland": 155805,
+  "Auckland": 1438446,
+  "Waikato": 413202,
+  "Bay of Plenty": 273360,
+  "Gisborne": 43938,
+  "Hawke's Bay": 156018,
+  "Taranaki": 110079,
+  "Manawatu / Wanganui": 225411,
+  "Wellington": 478290,
+  "Nelson / Tasman": 99702,
+  "Marlborough": 46302,
+  "West Coast": 36294,
+  "Canterbury": 554919,
+  "Otago": 223017,
+  "Southland": 97821
+};
+
+module.exports = {
+  population: population,
+  unemployment: unemployment
+};
