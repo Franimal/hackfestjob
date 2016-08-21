01ce6ce0bad5e222bdb8940e6796d41287eed35d
David Barnett <david@barnett.net.nz>
2016-08-21 15:20:38 +1200

    integrating region pages

--- routes/region.js
+++ routes/region.js
@@ -6,47 +6,58 @@ var apiIds = require('../data/trademe-ids.js');
 
 
 function requestNumListings(regionId, categoryId) {
-    var options = {
-        method: 'GET',
-        url: 'https://api.trademe.co.nz/v1/Search/Jobs.json',
-        qs: {category: categoryId, region: regionId},
-        headers: {
-            authorization: secret
-        },
-        json: true
-    };
+  var options = {
+    method: 'GET',
+    url: 'https://api.trademe.co.nz/v1/Search/Jobs.json',
+    qs: {category: categoryId, region: regionId},
+    headers: {
+      authorization: secret
+    },
+    json: true
+  };
 
-    return request(options).then((data) => {
-            return data.TotalCount;
-})
-    ;
+  return request(options).then((data) => {
+    return data.TotalCount;
+  })
+  ;
 }
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    console.log("Hello");
-    var category = req.query.category;
-    var location = req.query.location;
-    requestNumListings(100, 5001).then((data) => {
-        console.log(data);
-        var regionData = [{
-            jobs: 1000,
-            unemployed: 2,
-            ratio: '1:3',
-            population: 10000,
-            salary:1000
-        }];
+router.get('/:regionId', function (req, res, next) {
+  console.log("Hello");
+  var regionId = req.params.regionId;
+  var category = req.query.category;
+  apiIds.getLocationIds().then((locations) => {
+    console.log("wow so very");
+    console.log(locations);
+    var location = locations.find((l) => { return l.id == regionId });
+    console.log(location.name);
+    console.log("asd");
 
-        var job = [{
-            Location: "Auckland",
-            title: "How to excel in interviews",
-            employer: "KiwiBanks",
-            description: " This is a job interviw. ASKJSJKDSJ. aThelaskcncl alksdjioqwejl;wm;lme asjdkasd. askdjjksdjkashkqwenmn ",
-        }];
-        var categories = ["accounting", "engineering", "painting"];
-        res.render('region', { title: 'Express' , job:job, categories: categories, regions: regionData});
+    requestNumListings(regionId, category).then((data) => {
+      console.log(data);
+      var regionData = [{
+        jobs: 1000,
+        unemployed: 2,
+        ratio: '1:3',
+        population: 10000,
+        salary:1000
+      }];
 
+      var job = [{
+        Location: location.name,
+        title: "How to excel in interviews",
+        employer: "KiwiBanks",
+        description: " This is a job interviw. ASKJSJKDSJ. aThelaskcncl alksdjioqwejl;wm;lme asjdkasd. askdjjksdjkashkqwenmn ",
+      }];
+
+      apiIds.getJobCatagoryIds()
+        .then((categories) => {
+          res.render('region', { title: 'Express', location: location.name, job:job, categories: categories, regions: regionData, category: category, regionId: regionId});
+        });
     });
+
+  });
 
 });
 
